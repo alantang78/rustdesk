bd4b0e76239e62559b2a4d25306e8d159007cb6f
alantang78 <116530532+alantang78@users.noreply.github.com>
2024-09-23 19:18:02 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -101,7 +101,7 @@ const CHARS: &[char] = &[
 ];
 
 pub const RENDEZVOUS_SERVERS: &[&str] = &["rd.zaoanlonghui.cn"];
-pub const PUBLIC_RS_PUB_KEY: &str = "zaoanlonghui";
+pub const PUBLIC_RS_PUB_KEY: &str = "";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
